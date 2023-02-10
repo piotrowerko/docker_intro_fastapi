dcee92c63b576f8add057cfd151dee5387650d50
piotrowerko <piotrowerko@outlook.com>
2023-02-10 12:43:51 +0100

    docker integration

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Docker is a very powerful tool for developing applications that run in the cloud
 
 Today I'm going to show you how to do this, using a simple API server in Python as an example.
 
-Video: https://youtu.be/zkMRWDQV4Tg.
+Video: https://youtu.be/zkMRWcDQV4Tg.
+https://www.youtube.com/watch?v=zkMRWDQV4Tg&ab_channel=ArjanCodes
 
 ## Usage
 
@@ -19,6 +20,11 @@ This is how you run the code locally (without Docker):
 ```
 uvicorn main:app --host 0.0.0.0 --port 8080 --reload
 ```
+simple get:
+http://localhost:8080/
+simple get of channel:
+http://localhost:8080/channels/jackherrington
+
 
 Build and run the Docker image locally, as follows:
 
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass, field
 
 from fastapi import FastAPI, HTTPException, Response
 
+app = FastAPI()
+
 
 @dataclass
 class Channel:
@@ -12,26 +14,22 @@ class Channel:
     description: str = ""
 
 
-def main():
-    app = FastAPI()
-    channels: dict[str, Channel] = {}
+channels: dict[str, Channel] = {}
 
-    with open("channels.json", encoding="utf8") as file:
-        channels_raw = json.load(file)
-        for channel_raw in channels_raw:
-            channel = Channel(**channel_raw)
-            channels[channel.id] = channel
+with open("channels.json", encoding="utf8") as file:
+    channels_raw = json.load(file)
+    for channel_raw in channels_raw:
+        channel = Channel(**channel_raw)
+        channels[channel.id] = channel
 
-    @app.get("/")
-    def read_root() -> Response:
-        return Response("The server is running.")
 
-    @app.get("/channels/{channel_id}", response_model=Channel)
-    def read_item(channel_id: str) -> Channel:
-        if channel_id not in channels:
-            raise HTTPException(status_code=404, detail="Channel not found")
-        return channels[channel_id]
- 
-   
-if __name__ == '__main__':
-    main()
+@app.get("/")
+def read_root() -> Response:
+    return Response("The server is running!")
+
+
+@app.get("/channels/{channel_id}", response_model=Channel)
+def read_item(channel_id: str) -> Channel:
+    if channel_id not in channels:
+        raise HTTPException(status_code=404, detail="Channel not found")
+    return channels[channel_id]
